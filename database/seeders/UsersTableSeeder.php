1febd97deb0d40454fd8c478cf9fae9d1eecd1fd
PC-ERIC <eric.alcazarc@gmail.com>
2025-03-10 17:42:59 +0100

    Seeders todo bien

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -10,53 +10,68 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        // Crear usuarios con roles y sede
+        // Crear usuarios con roles, sede, estado y jefe_id
+
+        // Administrador
         User::create([
             'name' => 'Administrador',
             'email' => 'admin@empresa.com',
             'password' => Hash::make('qweQWE123'),
             'role_id' => 1,  // Administrador
             'sede_id' => 1,  // Barcelona
+            'estado' => 'activo',
         ]);
 
+        // Cliente Barcelona
         User::create([
             'name' => 'Cliente Barcelona',
             'email' => 'cliente@empresa.com',
             'password' => Hash::make('qweQWE123'),
             'role_id' => 2,  // Cliente
             'sede_id' => 1,  // Barcelona
+            'estado' => 'activo',
         ]);
 
+        // Técnico Barcelona
         User::create([
             'name' => 'Tecnico Barcelona',
             'email' => 'tecnico@empresa.com',
             'password' => Hash::make('qweQWE123'),
-            'role_id' => 4,  // Tecnico
+            'role_id' => 4,  // Técnico
             'sede_id' => 1,  // Barcelona
+            'jefe_id' => 1,  // Jefe de Barcelona (Administrador)
+            'estado' => 'activo',
         ]);
 
+        // Gestor Berlín
         User::create([
             'name' => 'Gestor Berlín',
             'email' => 'gestor@empresa.com',
             'password' => Hash::make('qweQWE123'),
             'role_id' => 3,  // Gestor equipo
             'sede_id' => 2,  // Berlín
+            'estado' => 'activo',
         ]);
 
+        // Técnico Berlín
         User::create([
             'name' => 'Tecnico Berlín',
             'email' => 'tecnico.berlin@empresa.com',
             'password' => Hash::make('qweQWE123'),
-            'role_id' => 4,  // Tecnico
+            'role_id' => 4,  // Técnico
             'sede_id' => 2,  // Berlín
+            'jefe_id' => 4,  // Gestor de Berlín
+            'estado' => 'activo',
         ]);
 
+        // Técnico Montreal
         User::create([
             'name' => 'Tecnico Montreal',
             'email' => 'tecnico.montreal@empresa.com',
             'password' => Hash::make('qweQWE123'),
-            'role_id' => 4,  // Tecnico
+            'role_id' => 4,  // Técnico
             'sede_id' => 3,  // Montreal
+            'estado' => 'activo',
         ]);
     }
 }
