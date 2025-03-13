2fcb628616265666ee20dae1b34a5c7f0a9ef6a4
PanDyyG <angel.camru@gmail.com>
2025-03-13 15:36:35 +0100

    cambio prioridad - Àngel

--- app/Models/Prioridad.php
+++ app/Models/Prioridad.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Prioridad extends Model {
     use HasFactory;
 
+    protected $table = 'prioridades';
+
     protected $fillable = ['nombre'];
 
     public function incidencias() {
